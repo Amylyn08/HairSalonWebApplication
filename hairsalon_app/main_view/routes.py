7c05ea6f97dd8537e6d3127ad2429cf3ad244307
Amy Nguyen <Amynguyen0132@gmail.com>
2024-04-20 00:07:55 -0400

    Finished with base html, working on main and about, will work on database class next

---
+++ hairsalon_app/main_view/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, flash, redirect, render_template, url_for
+
+
+main_bp = Blueprint('main', __name__, template_folder='templates')
+db = Database()
+
+@main_bp.route('/')
+@main_bp.route('/home/')
+def home():
+    return render_template('home.html')
+
+@main_bp.route('/about')
+def about():
+    return render_template('about.html')
