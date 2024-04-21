3736bbaaf02f284b1647c06138f937c4e278f0df
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-04-21 01:48:51 -0400

    Creates my proptype templates for the client/professional, + traced the route for listing those users in route.py.

--- hairsalon_app/users/routes.py
+++ hairsalon_app/users/routes.py
@@ -10,10 +10,18 @@ db = Database()
 users_bp = Blueprint("user",__name__,template_folder='templates')
 
 #List of users
-users = []
+users_clients= []
+users_professionals =[]
 
 #Generates all the notes
-@users_bp.route("/profile")
-def user_list():
+@users_bp.route("/profile/client")
+def user_list_client():
     """Function for listing all the users."""
-    
+    users = db.get_users_clients()
+    return render_template('client.html')
+
+@users_bp.route("/profile/professional")
+def user_list_professional():
+    """Function for listing all the users."""
+    users = db.get_users_professional()
+    return render_template('professional.html')
