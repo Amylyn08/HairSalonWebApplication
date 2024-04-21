367e8aac05d86802e8188521f656dee49f0a7dbf
dari44 <darina.hojeij@dawsoncollege.qc.ca>
2024-04-21 12:55:28 -0700

    my appointments html

--- hairsalon_app/appointment_view/routes.py
+++ hairsalon_app/appointment_view/routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, flash, redirect, render_template, url_for
 from hairsalon_app.appointment_view.forms import AppointmentForm
+from hairsalon_app.appointment_view.appointment import Appointment
 
 
 appointment_bp = Blueprint('appointment', __name__, template_folder='templates')
 #db = Database()
+
 
 @appointment_bp.route('/')
 @appointment_bp.route('/appointment/', methods=['POST', 'GET'])
@@ -15,3 +17,18 @@ def create_appointment():
 
     #return render_template("owners.html", context = owners, form = owners_form)
     return render_template('appointment.html', form = appointment_form)
+
+#route for a specific note
+@appointment_bp.route("/my_appointments")
+def my_appointments(): #the id is the one for the note
+    #mimic db for now
+    my_appointments = [
+        Appointment(1, "bob", "04-24-2024"),
+        Appointment(2, "bob", "04-27-2024"),
+        Appointment(3, "bob", "04-21-2024"),
+    ]
+     
+    if (len(my_appointments)!= 0):
+        return render_template("my_appointments.html", context = my_appointments)
+        
+    return redirect(url_for("create_appointment"))
