2db1a5ac612e8e10390a3437c476c56e8909db9a
Amy <amynguyen0132@gmail.com>
2024-04-24 23:00:20 -0400

    Working on register.html

--- hairsalon_app/users/forms.py
+++ hairsalon_app/users/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from flask_wtf.file import FileField, FileAllowed
 
@@ -23,5 +23,7 @@ class NewProfressionalForm(FlaskForm):
     user_image = FileField('user_image', validators=[FileAllowed(['png', 'jpg', 'jpeg'])])
     phone_number = StringField('phone_number', validators=[DataRequired()])
     address = StringField('address', validators=[DataRequired()])
-    age = StringField('age', validators=[DataRequired()])
+    age = IntegerField('age', validators=[DataRequired()])
+    pay_rate = StringField('pay_rate', validators=[DataRequired()])
+
     
