507ebefb618cdfdef3be55b001671ea448fc4338
Amy Nguyen <Amynguyen0132@gmail.com>
2024-04-28 00:10:47 -0400

    Adding different redirects per admins

--- hairsalon_app/main_view/routes.py
+++ hairsalon_app/main_view/routes.py
@@ -14,6 +14,20 @@ def home():
 def about():
     return render_template('about.html')
 
-@main_bp.route('/admin-home/')
-def admin_home():
-    return render_template('AdminLoggedIn.html')
+@main_bp.route('/adminsuper-home/')
+def adminsuper_home():
+    return render_template('home_adminsuper.html')
+
+@main_bp.route('/adminappoint-home/')
+def adminappoint_home():
+    return render_template('home_adminappoint.html')
+@main_bp.route('/adminuser-home/')
+def adminuser_home():
+    return render_template('home_adminuser.html')
+
+@main_bp.route('/member-home/')
+def adminsuper_home():
+    return render_template('home_member.html')
+
+
+
--- hairsalon_app/users/routes.py
+++ hairsalon_app/users/routes.py
@@ -104,8 +104,12 @@ def login():
                 user = User(username=form.username.data)
                 login_user(user)
                 flash(f'Success: Logged in as {form.username.data}', 'success')
-        if 'admin' in user_exists.user_type:
-            return redirect(url_for('main_bp.admin_home'))
+        if user_exists.user_type == 'admin_super':
+            return redirect(url_for('main_bp.adminsuper_home'))
+        elif user_exists.user_type == 'admin_appoint':
+            return redirect(url_for('main_bp.adminappoint_home'))
+        elif user_exists.user_type == 'admin_user':
+            return redirect(url_for('main_bp.adminuser_home'))
         else:
             return redirect(url_for('main_bp.home'))
     flash ('Invalid password or username. Retry', 'error')        
