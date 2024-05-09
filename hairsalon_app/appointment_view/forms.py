8efc8fc79d8612dc6cc2150f1067e2ffa2e0c914
Amy Nguyen <Amynguyen0132@gmail.com>
2024-05-08 23:02:22 -0400

    Added services choices in form to show real services

--- hairsalon_app/appointment_view/forms.py
+++ hairsalon_app/appointment_view/forms.py
@@ -6,6 +6,11 @@ from hairsalon_app.qdb.database import db
 
 pros_list = db.get_list_pros()
 choices = [(pro.username, pro.username) for pro in pros_list]
+
+service_list = db.get_all_services()
+service_names = [service[0] for service in service_list]
+choices_service = [(service, service) for service in service_names]
+
 # form to add Owner
 class AppointmentForm(FlaskForm):
     username = StringField('username', validators = [DataRequired()], render_kw={'readonly' : True})
@@ -13,12 +18,12 @@ class AppointmentForm(FlaskForm):
     date = DateField('date', validators = [DataRequired()])
     slot = SelectField('time', choices=[('10-11', '10-11'), ('11-12', '11-12'), ('12-13', '12-13'), ('13-14', '13-14'), ('14-15', '14-15'), ('15-16', '15-16'), ('16-17', '16-17')], validators = [DataRequired()])
     professional = SelectField('professional', choices=choices, validators = [DataRequired()]) #to change
-    service = SelectField('service', choices=[('Trimming','Trimming')], validators = [DataRequired()]) #to change
+    service = SelectField('service', choices=choices_service, validators = [DataRequired()]) #to change
     submit = SubmitField("Reserve")
 
 class AppointmentEditForm(FlaskForm):
     date = DateField('date', validators = [DataRequired()])
     slot = SelectField('time', choices=[('10-11', '10-11'), ('11-12', '11-12'), ('12-13', '12-13'), ('13-14', '13-14'), ('14-15', '14-15'), ('15-16', '15-16'), ('16-17', '16-17')], validators = [DataRequired()])
-    service = SelectField('service', choices=[('Trimming','Trimming')], validators = [DataRequired()]) #to change
+    service = SelectField('service', choices=choices_service, validators = [DataRequired()]) #to change
     status = SelectField('status', choices=[('pending','pending'), ('cancelled', 'cancelled')], validators = [DataRequired()])
     submit = SubmitField("Edit")
