d120aaf121edb6d71011a712fa43962f3a401e5e
Iana  Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-05-16 18:54:14 +0000

    Revert "Merge branch 'responsiveCSS' into 'main'"
    This reverts merge request !48

--- hairsalon_app/qdb/config_db.py
+++
@@ -1,5 +0,0 @@
-import os
-host= os.environ['ORA_HOST']
-usr=os.environ['ORA_USER']
-sn='pdbora19c.dawsoncollege.qc.ca'
-pw=os.environ['ORA_PASSWD']
--- run.py
+++
@@ -1,8 +0,0 @@
-from hairsalon_app.__init__ import create_app
-
-app = create_app()
-
-if __name__== '__main__':
-    app.run(port=5010, debug=True)
-else:
-    app.run(port=5010, debug=True)
