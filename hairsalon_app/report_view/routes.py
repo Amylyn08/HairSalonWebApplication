cb0aaa50b75cceb67e270f1646d03c0449110ec2
Amy <amynguyen0132@gmail.com>
2024-05-16 00:20:35 -0400

    Fixed bug in reports, data was not synced

--- hairsalon_app/report_view/routes.py
+++ hairsalon_app/report_view/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, flash, redirect, render_template, url_for
+import flask
 from flask_login import current_user, login_required
 from hairsalon_app.report_view.forms import ReportForm, ReportEdit
 from hairsalon_app.qdb.database import db
@@ -24,13 +25,27 @@ def create_report(appointment_id):
 @report_bp.route('/edit_report/<int:report_id>', methods=['POST', 'GET'])
 @login_required 
 def edit_report(report_id):
-    report = db.reports_cond(cond=f"WHERE report_id = {report_id} AND user_id = {current_user.user_id}")[0]
+    report = db.reports_cond(cond=f'''WHERE 
+                                            salon_report.APPOINTMENT_ID IN (
+                                                SELECT APPOINTMENT_ID FROM SALON_APPOINTMENT 
+                                                WHERE CLIENT_ID = {current_user.user_id} 
+                                                OR PROFESSIONAL_ID = {current_user.user_id}
+                                            )''')[0]
     if not report:
         flash('Report not found, or not your report.' 'info')
         return redirect(url_for('appointment_bp.specific_appointment', appointment_id=report.appointment_id))
     form = ReportEdit() 
     if form.validate_on_submit():
-        db.edit_report(report_id, form.client_report.data, form.professional_report.data)
+        if current_user.user_type == 'client':
+            client_report = form.client_report.data
+            pro_report = report.professional_report
+        elif current_user.user_type == 'professional':
+            client_report = report.client_report.data
+            pro_report = form.professional_report.data
+        else:
+            client_report = form.client_report.data        
+            pro_report = form.professional_report.data
+        db.edit_report(report_id, client_report, pro_report)
         flash('Report sent', 'success')
         return redirect(url_for('appointment_bp.specific_appointment', appointment_id=report.appointment_id))
     flash('Invalid Inputs.' 'error')
