3ae1592a0090d5c6201107ef6d03a03f77a8cf59
Amy <amynguyen0132@gmail.com>
2024-05-15 20:26:00 -0400

    Made it so that the professionals and the admins pick clients when making appointments, not professionals

--- hairsalon_app/appointment_view/routes.py
+++ hairsalon_app/appointment_view/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 import flask
 from flask_login import current_user, login_required
-from hairsalon_app.appointment_view.forms import AppointmentForm, AppointmentEditForm
+from hairsalon_app.appointment_view.forms import AppointmentForm, AppointmentEditForm, AppointmentFormPro
 from hairsalon_app.appointment_view.appointment import Appointment
 from hairsalon_app.qdb.database import db
 from markupsafe import escape
@@ -14,21 +14,42 @@ appointment_bp = Blueprint('appointment_bp', __name__, template_folder='template
 @login_required 
 def create_appointment():
     pros_list = db.get_members_cond(condition="user_type='professional'")
+    client_list = db.get_members_cond(condition="user_type='client'")
     service_list = db.services_cond()
-    form = AppointmentForm(pros_list, service_list) #create form to add address to list
-    if form.validate_on_submit():  # Check if form has been submitted
-        existing_apps = db.appointments_cond(cond=f"WHERE date_appointment = TO_DATE('{form.date.data}', 'YYYY-MM-DD') AND client_name = '{current_user.full_name}'")
+    
+    if current_user.user_type == 'professional' or 'admin' in current_user.user_type:
+        formPro = AppointmentFormPro()
+        formPro.client.choices = [(c.username, c.username) for c in client_list]
+        formPro.service.choices = [(s[1], s[1]) for s in service_list]
+        form = formPro
+    else:
+        formClient = AppointmentForm()
+        formClient.professional.choices = [(p.username, p.username) for p in pros_list]
+        formClient.service.choices = [(s[1], s[1]) for s in service_list]
+        form = formClient
+
+    if form.validate_on_submit():
+        existing_apps = db.appointments_cond(cond=f"WHERE date_appointment = TO_DATE('{form.date.data}', 'YYYY-MM-DD') AND client_name = '{current_user.full_name}'")    
         if len(existing_apps) == 0:       
-            db.add_new_appointment(username=current_user.username,
-                                   professional=form.professional.data,
-                                   service=form.service.data,
-                                   venue=form.venue.data,
-                                   slot=form.slot.data,
-                                   date=form.date.data)
+            if current_user.user_type == 'professional' or 'admin' in current_user.user_type:
+                user_data = form.client.data
+                pro_data = current_user.username
+            else:
+                user_data=current_user.username
+                pro_data = form.professional.data
+            db.add_new_appointment(
+                username=user_data,
+                professional=pro_data,
+                service=form.service.data,
+                venue=form.venue.data,
+                slot=form.slot.data,
+                date=form.date.data
+            )
             flash('Appointment scheduled', 'success')
             return redirect(url_for('appointment_bp.my_appointments', user_id=current_user.user_id))
         else:
-            flash('This appointment already exists.', 'error')  # Flash message for existing appointment
+            flash('This appointment already exists.', 'error')
+
     return render_template('appointment.html', form=form)
 
 
@@ -83,7 +104,8 @@ def edit_appointment(appointment_id):
         return redirect(url_for("appointment_bp.all_appointments"))
     service_list = db.services_cond()
     appointment = db.appointments_cond(cond=f"WHERE appointment_id = {appointment_id}")[0]
-    form = AppointmentEditForm(service_list)
+    form = AppointmentEditForm()
+    form.service.choices = [(s[1], s[1]) for s in service_list]
     if flask.request.method == 'GET':
         form.date.data = appointment.date_appointment
         form.slot.data = appointment.slot
